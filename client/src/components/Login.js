5502f44d400edb6335e79a858722e71e67005d14
archilk <archil.kumar@gmail.com>
2020-10-14 20:20:04 +0530

    Added signup functionality

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -114,7 +114,7 @@ export default function Login() {
         </Button>
         <Grid container justify="center">
           <Grid item>
-            <Link href="#" variant="body2">
+            <Link href="signup" variant="body2">
               {"Don't have an account? Sign Up"}
             </Link>
           </Grid>
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -14,6 +14,7 @@ import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import { MenuItem, Tab, Tabs } from '@material-ui/core';
+import { Alert } from '@material-ui/lab';
 
 const styles = (theme) => ({
   paper: {
@@ -43,10 +44,11 @@ class SignUp extends React.Component {
       age: 18,
       phone: '',
       city: '',
-      hospital: '',
-      specialty: '',
+      hospitalId: '',
+      specialtyId: '',
       allHospitals: [],
-      allSpecialties: []
+      allSpecialties: [],
+      success: null
     }
   }
 
@@ -58,8 +60,20 @@ class SignUp extends React.Component {
     .then((response) => this.setState({allSpecialties: response.data}), (error) => console.log(error));
   }
 
+  postSignup = () => {
+    axios.put(`/${this.state.userType}/register`, this.state).then(response => {
+      if (response.status === 200) {
+        this.setState({success: true});
+      } else {
+        this.setState({success: false});
+      }
+    }).catch(e => {
+      this.setState({success: false});
+    });
+  };
+
   render() {
-    const {userType, firstName, lastName, email, password, age, phone, city, hospital, specialty,
+    const {userType, firstName, lastName, email, password, age, phone, city, hospitalId, specialtyId,
       allHospitals, allSpecialties} = this.state;
     const {classes} = this.props;
     return (<Container component="main" maxWidth="xs">
@@ -82,142 +96,166 @@ class SignUp extends React.Component {
         </Tabs>
         <br />
         <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="fname"
-                name="firstName"
-                variant="outlined"
-                required
-                value={firstName}
-                onChange={event => this.setState({firstName: event.target.value})}
-                fullWidth
-                id="firstName"
-                label="First Name"
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                variant="outlined"
-                required
-                value={lastName}
-                onChange={event => this.setState({lastName: event.target.value})}
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                autoComplete="lname"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                value={email}
-                onChange={event => this.setState({email: event.target.value})}
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                value={password}
-                onChange={event => this.setState({password: event.target.value})}
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-            </Grid>
-            {userType === 'patient' ? <>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    variant="outlined"
-                    required
-                    value={age}
-                    onChange={event => this.setState({age: event.target.value})}
-                    select
-                    fullWidth
-                    name="age"
-                    label="Age"
-                    id="age"
-                  >
-                    {[...Array(101).keys()].slice(1).map(item => <MenuItem key={item} value={item}>{item}</MenuItem>)}
-                  </TextField>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    variant="outlined"
-                    required value={phone}
-                    onChange={event => this.setState({phone: event.target.value})}
-                    fullWidth
-                    name="phone"
-                    label="Phone"
-                    id="phone"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    variant="outlined"
-                    required
-                    value={city}
-                    onChange={event => this.setState({city: event.target.value})}
-                    fullWidth
-                    name="city"
-                    label="City"
-                    id="city"
-                  />
-                </Grid>
-              </> : <>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    variant="outlined"
-                    required
-                    value={hospital}
-                    onChange={event => this.setState({hospital: event.target.value})}
-                    select
-                    fullWidth
-                    name="hospital"
-                    label="Hospital"
-                    id="hospital"
-                  >
-                    {allHospitals.map(item => <MenuItem key={item.id} value={item.id}>{item.name}</MenuItem>)}
-                  </TextField>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    variant="outlined"
-                    required
-                    value={specialty}
-                    onChange={event => this.setState({specialty: event.target.value})}
-                    select
-                    fullWidth
-                    name="specialty"
-                    label="Specialty"
-                    id="specialty"
-                  >
-                    {allSpecialties.map(item => <MenuItem key={item.id} value={item.id}>{item.name}</MenuItem>)}
-                  </TextField>
-                </Grid>
-              </>}
+          {this.state.success === false ?
+          <Grid item xs={12}>
+            <Alert severity="error">Error in signup! Please contact support.</Alert>
           </Grid>
-          <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
-            Sign Up
-          </Button>
-          <Grid container justify="flex-end">
-            <Grid item>
-              <Link href="#" variant="body2">
-                Already have an account? Sign in
-              </Link>
+          : (this.state.success === true ?
+            <Grid item xs={12}>
+              <Alert severity="success">
+                Signup Successful! Please go to <Link href="login">Login page</Link>
+              </Alert>
             </Grid>
+            : null
+            )
+          }
+          <Grid item xs={12} sm={6}>
+            <TextField
+              autoComplete="fname"
+              name="firstName"
+              variant="outlined"
+              required
+              value={firstName}
+              onChange={event => this.setState({firstName: event.target.value})}
+              fullWidth
+              id="firstName"
+              label="First Name"
+              autoFocus
+            />
           </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              variant="outlined"
+              required
+              value={lastName}
+              onChange={event => this.setState({lastName: event.target.value})}
+              fullWidth
+              id="lastName"
+              label="Last Name"
+              name="lastName"
+              autoComplete="lname"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              variant="outlined"
+              required
+              value={email}
+              onChange={event => this.setState({email: event.target.value})}
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              variant="outlined"
+              required
+              value={password}
+              onChange={event => this.setState({password: event.target.value})}
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+          </Grid>
+          {userType === 'patient' ? <>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  variant="outlined"
+                  required
+                  value={age}
+                  onChange={event => this.setState({age: event.target.value})}
+                  select
+                  fullWidth
+                  name="age"
+                  label="Age"
+                  id="age"
+                >
+                  {[...Array(101).keys()].slice(1).map(item => <MenuItem key={item} value={item}>{item}</MenuItem>)}
+                </TextField>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  variant="outlined"
+                  required value={phone}
+                  onChange={event => this.setState({phone: event.target.value})}
+                  fullWidth
+                  name="phone"
+                  label="Phone"
+                  id="phone"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  value={city}
+                  onChange={event => this.setState({city: event.target.value})}
+                  fullWidth
+                  name="city"
+                  label="City"
+                  id="city"
+                />
+              </Grid>
+            </> : <>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  variant="outlined"
+                  required
+                  value={hospitalId}
+                  onChange={event => this.setState({hospitalId: event.target.value})}
+                  select
+                  fullWidth
+                  name="hospital"
+                  label="Hospital"
+                  id="hospital"
+                >
+                  {allHospitals.map(item => <MenuItem key={item.id} value={item.id}>{item.name}</MenuItem>)}
+                </TextField>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  variant="outlined"
+                  required
+                  value={specialtyId}
+                  onChange={event => this.setState({specialtyId: event.target.value})}
+                  select
+                  fullWidth
+                  name="specialty"
+                  label="Specialty"
+                  id="specialty"
+                >
+                  {allSpecialties.map(item => <MenuItem key={item.id} value={item.id}>{item.name}</MenuItem>)}
+                </TextField>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required value={phone}
+                  onChange={event => this.setState({phone: event.target.value})}
+                  fullWidth
+                  name="phone"
+                  label="Phone"
+                  id="phone"
+                />
+              </Grid>
+            </>}
+        </Grid>
+        <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit} onClick={this.postSignup}>
+          Sign Up
+        </Button>
+        <Grid container justify="flex-end">
+          <Grid item>
+            <Link href="login" variant="body2">
+              Already have an account? Sign in
+            </Link>
+          </Grid>
+        </Grid>
       </div>
     </Container>);
   }
